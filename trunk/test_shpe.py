5f07a75daf875f445f0f2904e6a861b48897759b
aleaxit <aleaxit@fd2f8a16-7455-0410-ac07-2d1190e0e8fb>
2008-08-29 15:20:31 +0000

    Started doing some tests & fixing the errors they reveal.

---
+++ trunk/test_shpe.py
@@ -0,0 +1,28 @@
+""" Tests of shpextract module using ca/zt06_d00 Shapefile (5-digits ZCTAs
+    in CA, freely supplied by the US Census as a TIGER/Line [TM] file).
+"""
+import doctest
+import shpextract
+
+def test_some_metas():
+  """ 
+  >>> s = shpextract.Shp('ca/zt06_d00.shp', id_field_name='ZCTA')
+  >>> len(s)
+  1678
+  >>> s.set_next_id('94303')
+  >>> r = s.get_next_record(id=1, recno=1, bbox=1, datalen=1, data=0)
+  >>> print r[0], r[1], r[3]
+  94303 2459 77
+  >>> print shpextract.showbb(r[2]).strip()
+  -122.1552   37.4162 -122.0780   37.5001
+  """
+
+def _test():
+    numfailures, numtests = doctest.testmod()
+    if numfailures == 0:
+      print '%d tests passed successfully' % numtests
+    # if there are any failures, doctest does its own reporting!-)
+
+if __name__ == "__main__":
+    _test()
+
